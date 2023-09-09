772088ab1d631d83520915399c4896500346cb9a
Ilya Lyamin <lyamin.ilya.s@gmail.com>
2023-09-09 22:20:43 +0400

    Edit doc title and add small text on home page

--- src/components/screens/Home/Home.jsx
+++ src/components/screens/Home/Home.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <>
+      <p>This is a set of mini-applications based on the React.js. I made them to study this framework.</p>
       <Link
         to="/counter"
         className="button">
