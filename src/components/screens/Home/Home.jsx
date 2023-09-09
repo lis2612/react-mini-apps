6f377cd744c830ffee8a3f092054fe91a4a9d91e
Ilya Lyamin <lyamin.ilya.s@gmail.com>
2023-09-09 17:32:39 +0400

    Small edits

--- src/components/screens/Home/Home.jsx
+++ src/components/screens/Home/Home.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div className="container">
+    <>
       <Link
         to="/counter"
         className="button">
@@ -13,7 +13,7 @@ function Home() {
         className="button">
         Modal
       </Link>
-    </div>
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.scss'
-import Router from './components/Router'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import Router from "./components/Router";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router/>
-  </React.StrictMode>,
-)
+    <div className="container">
+      <Router />
+    </div>
+  </React.StrictMode>
+);
