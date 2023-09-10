2d233f4a79f797e58d3b086130badac3ff1765ed
Ilya Lyamin <lyamin.ilya.s@gmail.com>
2023-09-10 14:20:08 +0400

    moved questions in separate file

---
+++ src/components/screens/Quiz/questions.js
@@ -0,0 +1,27 @@
+export const questions = [
+  {
+    title: "What is React.js?",
+    variants: ["framework", "library", "programming language"],
+    correct: 1,
+  },
+  {
+    title: "What is Vue.js?",
+    variants: ["framework", "library", "programming language"],
+    correct: 0,
+  },
+  {
+    title: "What doing hook useState?",
+    variants: ["nothing", "reload page", "get and set app state"],
+    correct: 2,
+  },
+  {
+    title: "Which programming language can use in Vue.js?",
+    variants: ["JavaScript", "JavaScript or TypeScript", "Python 3"],
+    correct: 1,
+  },
+  {
+    title: "What doing 'width' properties in CSS?",
+    variants: ["set window width", "get element width", "set element width"],
+    correct: 2,
+  },
+];
